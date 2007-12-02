67c4247e419d38e11cefe20f491c45e0cab590a0
Jordan Terrell <jterrell@wans.net>
2007-12-02 18:41:16 +0000

    Added extensions to System.Object to support easy reflection calls to methods via delegates...

---
+++ Application/iSynaptic.Commons/Extensions/ObjectExtensions/MethodReflectionObjectExtensions.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+namespace iSynaptic.Commons.Extensions.ObjectExtensions
+{
+    public static partial class ObjectExtensions
+    {
+        private static T GetDelegate<T>(string methodName, object target, params Type[] types)
+        {
+            Type targetType = target.GetType();
+
+            MethodInfo info = targetType.GetMethod(methodName,
+                                       BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance,
+                                       null,
+                                       types ?? Type.EmptyTypes,
+                                       null);
+
+            if (info != null)
+                return (T)(object)Delegate.CreateDelegate(typeof(T), target, info);
+            else
+                return default(T);
+        }
+
+        public static Func<TResult> GetFunc<TResult>(this object target, string methodName)
+        {
+            return GetDelegate<Func<TResult>>(methodName, target, null);
+        }
+
+        public static Func<T1, TResult> GetFunc<T1, TResult>(this object target, string methodName)
+        {
+            return GetDelegate<Func<T1, TResult>>(methodName, target, typeof(T1));
+        }
+
+        public static Func<T1, T2, TResult> GetFunc<T1, T2, TResult>(this object target, string methodName)
+        {
+            return GetDelegate<Func<T1, T2, TResult>>(methodName, target, typeof(T1), typeof(T2));
+        }
+
+        public static Func<T1, T2, T3, TResult> GetFunc<T1, T2, T3, TResult>(this object target, string methodName)
+        {
+            return GetDelegate<Func<T1, T2, T3, TResult>>(methodName, target, typeof(T1), typeof(T2), typeof(T3));
+        }
+
+        public static Func<T1, T2, T3, T4, TResult> GetFunc<T1, T2, T3, T4, TResult>(this object target, string methodName)
+        {
+            return GetDelegate<Func<T1, T2, T3, T4, TResult>>(methodName, target, typeof(T1), typeof(T2), typeof(T3), typeof(T4));
+        }
+
+        public static Action GetAction(this object target, string methodName)
+        {
+            return GetDelegate<Action>(methodName, target, null);
+        }
+
+        public static Action<T1> GetAction<T1>(this object target, string methodName)
+        {
+            return GetDelegate<Action<T1>>(methodName, target, typeof(T1));
+        }
+
+        public static Action<T1, T2> GetAction<T1, T2>(this object target, string methodName)
+        {
+            return GetDelegate<Action<T1, T2>>(methodName, target, typeof(T1), typeof(T2));
+        }
+
+        public static Action<T1, T2, T3> GetAction<T1, T2, T3>(this object target, string methodName)
+        {
+            return GetDelegate<Action<T1, T2, T3>>(methodName, target, typeof(T1), typeof(T2), typeof(T3));
+        }
+
+        public static Action<T1, T2, T3, T4> GetAction<T1, T2, T3, T4>(this object target, string methodName)
+        {
+            return GetDelegate<Action<T1, T2, T3, T4>>(methodName, target, typeof(T1), typeof(T2), typeof(T3), typeof(T4));
+        }
+    }
+}
