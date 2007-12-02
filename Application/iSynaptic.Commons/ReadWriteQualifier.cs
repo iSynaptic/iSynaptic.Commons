586eb58aa39d3fe9b068a8a9fbf7600e8bfb5198
Jordan Terrell <jterrell@wans.net>
2007-12-02 17:59:55 +0000

    ReadWriteQualifier now inherits from ReadableQualifier...

--- Application/iSynaptic.Commons/ReadWriteQualifier.cs
+++ Application/iSynaptic.Commons/ReadWriteQualifier.cs
@@ -4,45 +4,27 @@ using System.Text;
 
 namespace iSynaptic.Commons
 {
-    public partial class ReadWriteQualifier<Q, T>
+    public partial class ReadWriteQualifier<Q, T> : ReadableQualifier<Q, T>
     {
-        private Func<Q, T> _GetHandler = null;
         private Action<Q, T> _SetHandler = null;
-        private Func<Q[]> _GetKnownQualifiersHandler = null;
 
         public ReadWriteQualifier(Func<Q, T> getHandler, Action<Q, T> setHandler)
             : this(getHandler, setHandler, null)
         {
         }
 
-        public ReadWriteQualifier(Func<Q, T> getHandler, Action<Q, T> setHandler, Func<Q[]> getKnownQualifiersHandler)
+        public ReadWriteQualifier(Func<Q, T> getHandler, Action<Q, T> setHandler, Func<Q[]> getKnownQualifiersHandler) : base(getHandler, getKnownQualifiersHandler)
         {
-            if (getHandler == null)
-                throw new ArgumentNullException("getHandler");
-
             if (setHandler == null)
                 throw new ArgumentNullException("setHandler");
 
-            _GetHandler = getHandler;
             _SetHandler = setHandler;
-            _GetKnownQualifiersHandler = getKnownQualifiersHandler;
         }
 
-        public T this[Q qualifier]
+        public new T this[Q qualifier]
         {
-            get { return _GetHandler(qualifier); }
+            get { return base[qualifier]; }
             set { _SetHandler(qualifier, value); }
-        }
-
-        public Q[] KnownQualifiers
-        {
-            get
-            {
-                if (_GetKnownQualifiersHandler != null)
-                    return _GetKnownQualifiersHandler();
-                else
-                    return null;
-            }
         }
     }
 }
