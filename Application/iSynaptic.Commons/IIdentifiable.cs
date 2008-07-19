a2daa7a2ddea665c8f28ec6e45ac1d228e2f6e7e
Jordan Terrell <jterrell@wans.net>
2008-07-19 18:43:25 +0000

    Adding IIdentifiable<T> interface... Temporarily relaxing code coverage checking - this will be re-enabled when iSynaptic.SolutionBuild is integrated...

---
+++ Application/iSynaptic.Commons/IIdentifiable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iSynaptic.Commons
+{
+    public interface IIdentifiable<T> where T : IEquatable<T>
+    {
+        T GetIdentifier();
+    }
+}
