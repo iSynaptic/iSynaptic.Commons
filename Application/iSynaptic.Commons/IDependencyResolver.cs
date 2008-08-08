0131455fc6da2d9a1782e124ca5bfc9206afb97c
Jordan Terrell <jterrell@wans.net>
2008-08-08 17:30:00 +0000

    Added IDependencyResolver interface... Added ApiExtensions class to provide extension methods on top of iSynaptic.Commons members...

---
+++ Application/iSynaptic.Commons/IDependencyResolver.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iSynaptic.Commons
+{
+    public interface IDependencyResolver
+    {
+        object Resolve(Type dependencyType, object context);
+    }
+}
---
+++ Testing/iSynaptic.Commons.UnitTests/ApiExtensionsTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MbUnit.Framework;
+using Rhino.Mocks;
+
+namespace iSynaptic.Commons.UnitTests
+{
+    [TestFixture]
+    public class ApiExtensionsTests
+    {
+        [Test]
+        public void ResolveWithoutContext()
+        {
+            MockRepository mocks = new MockRepository();
+
+            var resolver = mocks.CreateMock<IDependencyResolver>();
+            Expect.Call(resolver.Resolve(typeof(int), null)).Return(1);
+            mocks.Replay(resolver);
+
+            resolver.Resolve<int>();
+
+            mocks.Verify(resolver);
+        }
+
+        [Test]
+        public void ResolveWithContext()
+        {
+            object context = new object();
+
+            MockRepository mocks = new MockRepository();
+
+            var resolver = mocks.CreateMock<IDependencyResolver>();
+            Expect.Call(resolver.Resolve(typeof(int), context)).Return(1);
+            mocks.Replay(resolver);
+
+            resolver.Resolve<int>(context);
+
+            mocks.Verify(resolver);
+        }
+    }
+}
