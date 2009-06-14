9e5b891a39d05f4e39323da1e0592d9f6c21e2ea
Jordan Terrell <jterrell@wans.net>
2009-06-14 03:59:23 +0000

    Replaced qualifier classes with interface definitions...

---
+++ Application/iSynaptic.Commons/IReadWriteQualifier.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace iSynaptic.Commons
+{
+    public interface IReadWriteQualifier<Q, T>
+    {
+        T this[Q qualifier] { get; set; }
+    }
+}
