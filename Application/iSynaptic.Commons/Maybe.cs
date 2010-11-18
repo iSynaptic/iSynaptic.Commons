9bad421d95b75041cf6622092b06bceb77b76a7d
Jordan Terrell <jterrell@wans.net>
2010-11-18 16:35:09 -0600

    Added basic Maybe<T> implementation...

---
+++ Application/iSynaptic.Commons/Maybe.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace iSynaptic.Commons
+{
+    public sealed class Maybe<T>
+    {
+        public static readonly Maybe<T> NoValue = new Maybe<T>(default(T)) { HasValue = false };
+
+        private T _Value = default(T);
+
+        public Maybe(T value)
+        {
+            HasValue = true;
+            Value = value;
+        }
+
+        public bool HasValue { get; private set; }
+        public T Value
+        {
+            get
+            {
+                if(HasValue != true)
+                    throw new InvalidOperationException("Maybe object must have a value.");
+
+                return _Value;
+            }
+            private set { _Value = value; }
+        }
+    }
+}
---
+++ Testing/iSynaptic.Commons.UnitTests/MaybeTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace iSynaptic.Commons
+{
+    [TestFixture]
+    public class MaybeTests
+    {
+        [Test]
+        public void AccessingValueOnNoValueThrowsException()
+        {
+            Assert.Throws<InvalidOperationException>(() => Console.WriteLine(Maybe<string>.NoValue.Value));
+        }
+
+        [Test]
+        public void HasValueOnNoValueReturnsFalse()
+        {
+            Assert.IsFalse(Maybe<string>.NoValue.HasValue);
+        }
+
+        [Test]
+        public void HasValueOnAValueReturnsTrue()
+        {
+            Assert.IsTrue(new Maybe<string>(null).HasValue);
+        }
+
+        [Test]
+        public void AccessingValueOnAValueReturnsExpectedValue()
+        {
+            Assert.IsNull(new Maybe<string>(null).Value);
+            Assert.AreEqual("Hello, World!", new Maybe<string>("Hello, World!").Value);
+        }
+    }
+}
