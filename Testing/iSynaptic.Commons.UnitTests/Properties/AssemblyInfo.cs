160a0ff30a5e231275e8a9a7bf82b86c3ed1b3bc
Jordan Terrell <jterrell@wans.net>
2010-11-22 12:55:48 -0600

    Changes all assemblies to be marked as CLS compliant...

--- Testing/iSynaptic.Commons.UnitTests/Properties/AssemblyInfo.cs
+++ Testing/iSynaptic.Commons.UnitTests/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -18,6 +19,7 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("42be6c52-6ced-4bca-b7b9-5e0bfe9df350")]
