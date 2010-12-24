9ed3fea64b21e908b4a106dc708bb4a165c49147
Jordan Terrell <jterrell@wans.net>
2010-12-23 23:31:48 -0600

    Added the ability to merge handwritten il into Commons assembly; created Cast class...

---
+++ Testing/iSynaptic.Commons.UnitTests/CastTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace iSynaptic.Commons
+{
+    [TestFixture]
+    public class CastTests
+    {
+        [Test]
+        public void Cast_ValueType_ReturnsCorrectly()
+        {
+            Assert.AreEqual(42, CastToInt(42));
+        }
+
+        [Test]
+        public void Cast_ReferenceType_ReturnsCorrectly()
+        {
+            Assert.AreEqual("Hello, World!", CastToString("Hello, World!"));
+
+        }
+
+        private int CastToInt<T>(T source)
+        {
+            return Cast.To<T, int>(source);
+        }
+
+        private string CastToString<T>(T source)
+        {
+            return Cast.To<T, string>(source);
+        }
+    }
+}
