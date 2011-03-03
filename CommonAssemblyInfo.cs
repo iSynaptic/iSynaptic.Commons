16fdec0731768acf0f752ce79704ca27a42edd73
Jordan Terrell <jterrell@wans.net>
2011-03-03 17:57:58 -0600

    Incremented version number...

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 
-[assembly: AssemblyVersion("0.1.0.11")]
-[assembly: AssemblyFileVersion("0.1.0.11")]
+[assembly: AssemblyVersion("0.1.0.12")]
+[assembly: AssemblyFileVersion("0.1.0.12")]
