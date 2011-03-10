a5438f75cca6c00c56f93efa2dbcf01cb9d752ff
Jordan Terrell <jterrell@wans.net>
2011-03-10 11:14:15 -0600

    Added ToCovariantExpression extension method...

---
+++ Application/iSynaptic.Commons/ExpressionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace iSynaptic.Commons
+{
+    public static class ExpressionExtensions
+    {
+        public static Expression<Func<T1, TCovariantResult>> ToCovariantExpression<T1, TResult, TCovariantResult>(this Expression<Func<T1, TResult>> self)
+            where TResult : TCovariantResult
+        {
+            return Expression.Lambda<Func<T1, TCovariantResult>>(self.Body, self.Parameters);
+        }
+    }
+}
---
+++ Testing/iSynaptic.Commons.UnitTests/ExpressionExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using NUnit.Framework;
+
+namespace iSynaptic.Commons
+{
+    [TestFixture]
+    public class ExpressionExtensionsTests
+    {
+        [Test]
+        public void ToCovariant_WhenCompiled_ExecutesCorrectly()
+        {
+            Expression<Func<string, string>> expr = x => x.ToUpper();
+            Expression<Func<string, object>> covariantExpr = expr.ToCovariantExpression<string, string, object>();
+
+            Func<string, object> covariantFunc = covariantExpr.Compile();
+            Assert.AreEqual("HELLO", covariantFunc("hello"));
+        }
+    }
+}
