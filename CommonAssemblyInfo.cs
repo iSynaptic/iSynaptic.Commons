3b57b9798b67c15b1288f7bb9b4226053cd176bd
Jordan Terrell <jterrell@wans.net>
2011-03-11 12:22:30 -0600

    Changed license to MIT license; incremented version number...

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 
-[assembly: AssemblyVersion("0.1.0.18")]
-[assembly: AssemblyFileVersion("0.1.0.18")]
+[assembly: AssemblyVersion("0.1.0.19")]
+[assembly: AssemblyFileVersion("0.1.0.19")]
