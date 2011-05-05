cd1d12d5968077912309c4a4ecfd920f46e9ac21
Jordan Terrell <jterrell@wans.net>
2011-05-04 20:36:48 -0500

    Moved ExpressionExtensions into Linq namespace...

--- Testing/iSynaptic.Commons.UnitTests/Linq/Expressions/ExpressionExtensionsTests.cs
+++ Testing/iSynaptic.Commons.UnitTests/Linq/Expressions/ExpressionExtensionsTests.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
 using NUnit.Framework;
 
-namespace iSynaptic.Commons
+namespace iSynaptic.Commons.Linq.Expressions
 {
     [TestFixture]
     public class ExpressionExtensionsTests
