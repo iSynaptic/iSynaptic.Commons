a513c63d84a9b017b8d8fe041e1c721866fe7a8f
Jordan Terrell <jterrell@wans.net>
2011-06-29 06:42:38 -0500

    Updated version number...

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 
-[assembly: AssemblyVersion("0.1.0.41")]
-[assembly: AssemblyFileVersion("0.1.0.41")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
