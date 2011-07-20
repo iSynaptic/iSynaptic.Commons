219457e864b742a7e23e21a58e4422e7b0053259
Jordan Terrell <jterrell@wans.net>
2011-07-20 16:25:12 -0500

    Yet another tweak to AttributeExodataBindingSource...

--- Application/iSynaptic.Commons.Data/AttributeExodataBindingSource.cs
+++ Application/iSynaptic.Commons.Data/AttributeExodataBindingSource.cs
@@ -13,10 +13,14 @@ namespace iSynaptic.Commons.Data
         {
             Guard.NotNull(request, "request");
 
-            ICustomAttributeProvider provider = request.Member ?? typeof(TSubject);
+            ICustomAttributeProvider provider = request.Member ?? 
+                request.Subject
+                .Where(x => x != null)
+                .Select(x => x.GetType())
+                .ValueOrDefault(typeof(TSubject));
 
             return provider.GetAttributesOfType<IExodataAttribute<TExodata>>()
-                .Select(x => ExodataBinding.Create<TExodata, object, TSubject>(null, this, Maybe<ISymbol>.NoValue, Maybe<object>.NoValue, Maybe<TSubject>.NoValue, request.Member != null ? new[]{request.Member} : null, null, r => x.TryResolve(r).AsMaybe()));
+                .Select(x => ExodataBinding.Create<TExodata, object, TSubject>(null, this, Maybe<ISymbol>.NoValue, Maybe<object>.NoValue, request.Subject, request.Member != null ? new[]{request.Member} : null, null, r => x.TryResolve(r).AsMaybe()));
         }
     }
 }
