cae4b54316f5e91d8a6b915aec57cd7c1a9c6990
Jordan Terrell <jterrell@wans.net>
2012-01-29 22:08:18 -0600

    Added IIndexable interface...

---
+++ Application/iSynaptic.Commons/Collections/Generic/IIndexable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace iSynaptic.Commons.Collections.Generic
+{
+    public interface IIndexable : IEnumerable
+    {
+        object this[int index] { get; }
+        int Length { get; }
+    }
+
+    public interface IIndexable<out T> : IIndexable, IEnumerable<T>
+    {
+        new T this[int index] { get; }
+    }
+}
