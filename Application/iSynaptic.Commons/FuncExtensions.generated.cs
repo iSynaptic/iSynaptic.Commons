f35cba4898c7f6917372d8fa5f51390191937e4a
Jordan Terrell <jterrell@wans.net>
2012-02-14 09:44:09 -0600

    Added license header to generated file; deleted orphaned file...

--- Application/iSynaptic.Commons/FuncExtensions.generated.cs
+++ Application/iSynaptic.Commons/FuncExtensions.generated.cs
@@ -1,4 +1,4 @@
-// The MIT License
+﻿// The MIT License
 // 
 // Copyright (c) 2011 Jordan E. Terrell
 // 
@@ -19,8 +19,6 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-
-
 
 using System;
 using System.Collections.Generic;
