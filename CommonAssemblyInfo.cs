5bc6aaa63cbb199a7eee112a6219fe31237ebcb9
Jordan Terrell <jterrell@wans.net>
2014-01-21 14:48:50 -0600

    Updated release notes, Nuget spec, and version info...

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿// The MIT License
 // 
-// Copyright (c) 2012-2013 Jordan E. Terrell
+// Copyright (c) 2012-2014 Jordan E. Terrell
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -30,11 +30,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("iSynaptic")]
 [assembly: AssemblyTrademark("iSynaptic")]
 [assembly: AssemblyProduct("iSynaptic.Commons")]
-[assembly: AssemblyCopyright("Copyright © Jordan Terrell 2012-2013")]
+[assembly: AssemblyCopyright("Copyright © Jordan Terrell 2012-2014")]
 
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 
-[assembly: AssemblyVersion("0.4.25.0")]
-[assembly: AssemblyFileVersion("0.4.25.0")]
-[assembly: AssemblyInformationalVersion("0.4.25.0")]
+[assembly: AssemblyVersion("0.4.26.0")]
+[assembly: AssemblyFileVersion("0.4.26.0")]
+[assembly: AssemblyInformationalVersion("0.4.26.0")]
